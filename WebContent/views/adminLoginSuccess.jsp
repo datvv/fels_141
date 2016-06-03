60d3b629ef27cb9466537115bdc491d6c8c7071a
datvv <vuvandat@framgia0166-lt.framgia.com>
2016-06-04 03:35:45 +0700

    create session, cookie when user login. remove session, cookie when user logout

--- WebContent/views/adminLoginSuccess.jsp
+++ WebContent/views/adminLoginSuccess.jsp
@@ -1,5 +1,6 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
-    pageEncoding="UTF-8"%>
+	pageEncoding="UTF-8"%>
+<%@ taglib prefix="s" uri="/struts-tags" %>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
@@ -8,5 +9,6 @@
 </head>
 <body>
 	<h3>This is admin page</h3>
+	<s:a action="logoutAction">Logout</s:a>
 </body>
 </html>
--- src/framgiavn/project01/web/action/UserLoginAction.java
+++ src/framgiavn/project01/web/action/UserLoginAction.java
@@ -3,8 +3,18 @@
  */
 package framgiavn.project01.web.action;
 
+import java.util.Map;
+
+import javax.servlet.ServletResponse;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.apache.commons.validator.EmailValidator;
 import org.apache.struts2.dispatcher.SessionMap;
+import org.apache.struts2.interceptor.ServletRequestAware;
+import org.apache.struts2.interceptor.ServletResponseAware;
+import org.apache.struts2.interceptor.SessionAware;
 
 import com.opensymphony.xwork2.ActionSupport;
 
@@ -17,18 +27,47 @@ import framgiavn.project01.web.model.User;
  * @author datvv
  * 
  */
-public class UserLoginAction extends ActionSupport {
+public class UserLoginAction extends ActionSupport implements SessionAware, ServletRequestAware, ServletResponseAware {
 
 	private UserBusiness userBusiness = null;
 	private User user;
 	private String message = "";
+	private User userLogin;
+	private SessionMap<String, Object> session;
+	private boolean cbRemember = false;
+
+	protected HttpServletRequest servletRequest;
+	protected HttpServletResponse servletResponse;
+
+	/**
+	 * Login required to system use cookie
+	 * 
+	 * @return input: return the login page, addminlogin : return addmin page,
+	 *         userlogin: return user page, error: return page error
+	 */
+	@Override
+	public String execute() throws Exception {
+		User userCookie = checkCookieUser();
+		if (userCookie == null) {
+			return INPUT;
+		} else {
+			User userCk = userBusiness.checkExistUser(userCookie);
+			if (userCk != null && userCk.getIsAdmin()) {
+				return "adminlogin";
+			} else if (userCk != null && !userCk.getIsAdmin()) {
+				return "userlogin";
+			} else {
+				return ERROR;
+			}
+		}
+	}
 
 	/**
 	 * Check exist user in database;
 	 * 
-	 * @return input: return page input with error input, addminlogin : return
-	 *         addmin page, userlogin: return user page, error: return page
-	 *         error
+	 * @return input: return the login page with input errors, addminlogin :
+	 *         return addmin page, userlogin: return user page, error: return
+	 *         page error
 	 * 
 	 */
 	public String checkUserLogin() {
@@ -38,10 +77,22 @@ public class UserLoginAction extends ActionSupport {
 			return INPUT;
 		}
 		try {
-			User userCheck = userBusiness.checkExistUser(user);
-			if (userCheck != null) {
-
-				if (userCheck.getIsAdmin()) {
+			userLogin = userBusiness.checkExistUser(user);
+			if (userLogin != null) {
+				// create session
+				session.put("email", userLogin.getEmail());
+				// check tick remember me
+				if (this.cbRemember) {
+					// set cookie for email
+					Cookie ckEmail = new Cookie("email", userLogin.getEmail());
+					ckEmail.setMaxAge(3600);
+					servletResponse.addCookie(ckEmail);
+					// set cookie for password
+					Cookie ckPassword = new Cookie("password", userLogin.getPassword());
+					ckPassword.setMaxAge(3600);
+					servletResponse.addCookie(ckPassword);
+				}
+				if (userLogin.getIsAdmin()) {
 					return "adminlogin";
 				} else {
 					return "userlogin";
@@ -55,6 +106,33 @@ public class UserLoginAction extends ActionSupport {
 			return ERROR;
 		}
 
+	}
+
+	/**
+	 * Check the existence of cookie
+	 * 
+	 * @return user: cookie user , null:not existence of cookie user
+	 */
+
+	public User checkCookieUser() {
+		User user = null;
+		String email = "";
+		String password = "";
+		for (Cookie ck : servletRequest.getCookies()) {
+			if (ck.getName().equalsIgnoreCase("email")) {
+				email = ck.getValue();
+			}
+			if (ck.getName().equalsIgnoreCase("password")) {
+				password = ck.getValue();
+			}
+		}
+		if (!email.isEmpty() && !password.isEmpty()) {
+			user = new User();
+			user.setEmail(email);
+			user.setPassword(password);
+
+		}
+		return user;
 	}
 
 	public void setUserBusiness(UserBusiness userBusiness) {
@@ -75,6 +153,37 @@ public class UserLoginAction extends ActionSupport {
 
 	public void setMessage(String message) {
 		this.message = message;
+	}
+
+	@Override
+	public void setSession(Map<String, Object> map) {
+		session = (SessionMap<String, Object>) map;
+	}
+
+	public SessionMap<String, Object> getSession() {
+		return session;
+	}
+
+	public void setSession(SessionMap<String, Object> session) {
+		this.session = session;
+	}
+
+	public boolean isCbRemember() {
+		return cbRemember;
+	}
+
+	public void setCbRemember(boolean cbRemember) {
+		this.cbRemember = cbRemember;
+	}
+
+	@Override
+	public void setServletResponse(HttpServletResponse servletResponse) {
+		this.servletResponse = servletResponse;
+	}
+
+	@Override
+	public void setServletRequest(HttpServletRequest servletRequest) {
+		this.servletRequest = servletRequest;
 	}
 
 }
